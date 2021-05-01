2c9981ab1f03a5fcf913c0012a3801b5bc0d37c6
uddeepk <uddeepk@gmail.com>
2021-04-30 23:00:11 -0800

    put inside loop to check input

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -1,25 +1,30 @@
 use std::io;
 
 fn main() {
-    println!("Nth Fibonacci number");
-    println!("Enter a number: ");
+    
+    loop {
+	println!("Nth Fibonacci number ( n starting at 0)");
+	println!("Enter a number: ");
 
-    let mut n = String::new();
+	 let mut n = String::new();
 
-    io::stdin()
-	.read_line(&mut n)
-	.expect("Failed to read line");
+	io::stdin()
+	    .read_line(&mut n)
+	    .expect("Failed to read line");
 
-    let n: i32 = n.trim().parse().unwrap();
-   /* let n: i32 = match n.trim().parse() {
-	Ok(num) => num,
-	Err(_) => ,
-    };*/
+	//let n: i32 = n.trim().parse().unwrap();
+	let n: i32 = match n.trim().parse() {
+	    Ok(num) => num,
+	    Err(_) => continue,
+	};
 
-    let nth_fib = fib(n);
-    //    let nth_fib
+	let nth_fib = fib(n);
 
-    println!("The {}th Fibonacci number is {}", n, nth_fib);
+	println!("The {}th Fibonacci number is {}", n, nth_fib);
+
+	break;
+    }
+   
 }
 
 fn fib(n: i32) -> i32 {
