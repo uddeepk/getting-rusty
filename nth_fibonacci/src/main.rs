4483a740c66ddc2fdd77c77ffc0f0444d7c2bf45
uddeepk <uddeepk@gmail.com>
2021-05-11 12:28:02 -0800

    fmt

--- nth_fibonacci/src/main.rs
+++ nth_fibonacci/src/main.rs
@@ -1,39 +1,35 @@
 use std::io;
 
 fn main() {
-    
     loop {
-	println!("Nth Fibonacci number ( n starting at 0)");
-	println!("Enter a number: ");
+        println!("Nth Fibonacci number ( n starting at 0)");
+        println!("Enter a number: ");
 
-	 let mut n = String::new();
+        let mut n = String::new();
 
-	io::stdin()
-	    .read_line(&mut n)
-	    .expect("Failed to read line");
+        io::stdin().read_line(&mut n).expect("Failed to read line");
 
-	//let n: i32 = n.trim().parse().unwrap();
-	let n: i32 = match n.trim().parse() {
-	    Ok(num) => num,
-	    Err(_) => continue,
-	};
+        //let n: i32 = n.trim().parse().unwrap();
+        let n: i32 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-	let nth_fib = fib(n);
+        let nth_fib = fib(n);
 
-	println!("The {}th Fibonacci number is {}", n, nth_fib);
+        println!("The {}th Fibonacci number is {}", n, nth_fib);
 
-	break;
+        break;
     }
-   
 }
 
 fn fib(n: i32) -> i32 {
     if n == 0 {
-	return 0;
+        return 0;
     }
-    if n == 1  {
-	return 1;
+    if n == 1 {
+        return 1;
     }
     // else
-    return fib(n - 1) + fib ( n - 2);
+    return fib(n - 1) + fib(n - 2);
 }
